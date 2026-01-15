1cb70a46d13e5530ec2224b4eccc8f43e242f7aa
Matt Ensor <hi@mattensor.io>
2026-01-15 11:18:56 +1000

    feat: when canvas is initialised and resized ensure canvas bitmap size is correct

--- src/components/CanvasHost/CanvasHost.tsx
+++ src/components/CanvasHost/CanvasHost.tsx
@@ -1,10 +1,43 @@
 import type { PointerEvent } from "react"
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 import { CanvasArea } from "@/components/CanvasArea"
 import styles from "./styles.module.css"
 
 export function CanvasHost() {
+	const hostRef = useRef<HTMLDivElement>(null)
 	const canvasRef = useRef<HTMLCanvasElement>(null)
+
+	useEffect(() => {
+		const host = hostRef.current
+		if (host == null) return
+
+		function onResize(_entries: ResizeObserverEntry[]) {
+			const canvas = canvasRef.current
+			if (canvas == null) return
+
+			const context = canvas.getContext("2d")
+			if (context == null) return
+
+			// canvas bounds in viewport coordinates
+			const canvasRectangle = canvas.getBoundingClientRect()
+
+			// ensure css pixles match device pixels
+			const devicePixelRatio = window.devicePixelRatio || 1
+			canvas.width = Math.floor(canvasRectangle.width * devicePixelRatio)
+			canvas.height = Math.floor(canvasRectangle.height * devicePixelRatio)
+
+			// ensure drawing is scaled
+			context.setTransform(devicePixelRatio, 0, 0, devicePixelRatio, 0, 0)
+		}
+
+		const resizeObserver = new ResizeObserver(onResize)
+
+		resizeObserver.observe(host)
+
+		return () => {
+			resizeObserver.disconnect()
+		}
+	}, [])
 
 	function onPointerDown(event: PointerEvent<HTMLCanvasElement>) {
 		const canvas = canvasRef.current
@@ -15,14 +48,6 @@ export function CanvasHost() {
 
 		// canvas bounds in viewport coordinates
 		const canvasRectangle = canvas.getBoundingClientRect()
-
-		// ensure css pixles match device pixels
-		const devicePixelRatio = window.devicePixelRatio || 1
-		canvas.width = Math.floor(canvasRectangle.width * devicePixelRatio)
-		canvas.height = Math.floor(canvasRectangle.height * devicePixelRatio)
-
-		// ensure drawing is scaled
-		context.setTransform(devicePixelRatio, 0, 0, devicePixelRatio, 0, 0)
 
 		// pointer position in viewport coordinates
 		const xClickPosition = event.clientX
@@ -39,7 +64,7 @@ export function CanvasHost() {
 	}
 
 	return (
-		<div className={styles.canvasHost}>
+		<div ref={hostRef} className={styles.canvasHost}>
 			<CanvasArea ref={canvasRef} onPointerDown={onPointerDown} />
 		</div>
 	)
